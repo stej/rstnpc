b1925f94578851d064b26ea576a9b8a260f531c7
Josef Stefan <j.stefan@quadient.com>
2023-10-03 20:32:41 +0200

    01 hw - unstoppable

--- 01/hello_world/src/main.rs
+++ 01/hello_world/src/main.rs
@@ -1,3 +1,15 @@
+use std::{thread, time::Duration};
+
 fn main() {
-    println!("Hello, world!");
+    ctrlc::set_handler(move || {
+        println!("Unstopable!!");
+    })
+    .expect("Unable to run forever");
+
+    for _ in 1..  {
+        println!("Hello, world!");
+        // looks like sleeping is needed to get the ctrl-c to work
+        thread::sleep(Duration::from_millis(5));
+    }
+    
 }
