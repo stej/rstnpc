e3542e9c2930d82932016473332136f60f2a66b4
Josef Stefan <j.stefan@quadient.com>
2023-10-27 16:35:49 +0200

    07 hw

--- 01/hello_world/src/main.rs
+++ 01/hello_world/src/main.rs
@@ -1,9 +1,13 @@
 mod csv;
+mod operation;
 
 use regex::Regex;
 use std::{process, env};
-use std::io::Read;
+use std::io::{Read, stdin};
 use std::error::Error;
+use operation::{Operation, OperationWithParam};
+use std::thread;
+
 
 static OPTIONS: &str = "lowercase|uppercase|slugify|no-spaces|len|reverse|csv";
 
@@ -52,31 +56,81 @@ fn csv() -> Result<String, Box<dyn Error>> {
     Ok(csv.to_string())
 }
 
+fn split_line_to_operation_and_arg(line: &str) -> Result<OperationWithParam, Box<dyn Error>> {
+    let mut split = line.splitn(2, ' ');
+    let operation =
+        split
+        .next().ok_or("No operation found")?
+        .parse::<Operation>()?;
+    let arg = split
+        .next()
+        .ok_or("No argument found")?;
+    Ok(OperationWithParam{operation, param: arg.trim().to_string()})
+}
+
 
 fn main() {
     let args: Vec<String> = env::args().collect();
-    if args.len() != 2 {
-        usage();
-    }
-    let option = args[1].as_str();
-    let args_regex = Regex::new(format!(r"^({})$", OPTIONS).as_str()).unwrap();
-    if !args_regex.is_match(option) {
-        usage();
-    }
-    
-    let result =
-        match option {
-            "lowercase" => lowercase(),
-            "uppercase" => uppercase(),
-            "slugify" => slugify(),
-            "no-spaces" => no_space(),
-            "len" => len(),
-            "reverse" => reverse(),
-            "csv" => csv(),
-            _ => panic!("Unknown option")
+
+    if args.len() > 1 {
+        if args.len() != 2 {
+            usage();
+        }
+        let option = args[1].as_str();
+        let args_regex = Regex::new(format!(r"^({})$", OPTIONS).as_str()).unwrap();
+        if !args_regex.is_match(option) {
+            usage();
+        }
+        
+        let result =
+            match option {
+                "lowercase" => lowercase(),
+                "uppercase" => uppercase(),
+                "slugify" => slugify(),
+                "no-spaces" => no_space(),
+                "len" => len(),
+                "reverse" => reverse(),
+                "csv" => csv(),
+                _ => panic!("Unknown option")
+            };
+        return match result {
+            Ok(transmuted) => println!("{}", transmuted),
+            Err(err) => eprintln!("Operation '{}' failed: {}", option, err)
         };
-    match result {
-        Ok(transmuted) => println!("{}", transmuted),
-        Err(err) => eprintln!("Operation '{}' failed: {}", option, err)
     }
+
+    let (send, rec) = std::sync::mpsc::channel();
+    let input_thread = thread::spawn(move || {
+        let mut line = String::new();
+        loop {
+            stdin().read_line(&mut line).unwrap();
+            match split_line_to_operation_and_arg(&line) {
+                Ok(op) => send.send(op).unwrap(),
+                Err(err) => eprintln!("Error: {}", err)
+            }
+            line.clear()
+        }
+    });
+    let process_thread = thread::spawn(move || {
+        loop {
+            let operation = rec.recv().unwrap();
+            // let result =
+            //     match operation {
+            //         Operation::Lowercase => lowercase(),
+            //         Operation::Uppercase => uppercase(),
+            //         Operation::Slugify => slugify(),
+            //         Operation::NoSpaces => no_space(),
+            //         Operation::Len => len(),
+            //         Operation::Reverse => reverse(),
+            //         Operation::Csv => csv(),
+            //     };
+            // match result {
+            //     Ok(transmuted) => println!("{}", transmuted),
+            //     Err(err) => eprintln!("Operation '{:?}' failed: {}", operation, err)
+            // };
+            println!("{:?}", operation);
+        }
+    });
+    input_thread.join().unwrap();
+    process_thread.join().unwrap();
 }
---
+++ 01/hello_world/src/operation.rs
@@ -0,0 +1,45 @@
+use std::str::FromStr;
+
+#[derive(Debug, PartialEq)]
+pub enum Operation {
+    Lowercase,
+    Uppercase,
+    Slugify,
+    NoSpaces,
+    Len,
+    Reverse,
+    Csv
+}
+
+impl FromStr for Operation {
+    type Err = String;
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        match s {
+            "lowercase" => Ok(Operation::Lowercase),
+            "uppercase" => Ok(Operation::Uppercase),
+            "slugify" => Ok(Operation::Slugify),
+            "no-spaces" => Ok(Operation::NoSpaces),
+            "len" => Ok(Operation::Len),
+            "reverse" => Ok(Operation::Reverse),
+            "csv" => Ok(Operation::Csv),
+            _ => Err(format!("Unknown operation: {}", s))
+        }
+    }
+}
+
+#[derive(Debug)]
+pub struct OperationWithParam {
+    pub operation: Operation,
+    pub param: String,
+}
+
+#[cfg(test)]
+mod tests_enum_fromstr {
+
+    use crate::Operation;
+
+    #[test]
+    fn parse_enum() {
+        assert_eq!(Ok(Operation::Len), "len".parse());
+    }
+}
