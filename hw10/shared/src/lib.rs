429cd29720274f5c8248c24b88392cc2ec550cbe
Josef Stefan <j.stefan@quadient.com>
2023-11-01 11:28:48 +0100

    hw 10 - chat app

---
+++ hw10/shared/src/lib.rs
@@ -0,0 +1,34 @@
+use serde::{Deserialize, Serialize};
+use bincode::Error as BincodeError;
+
+#[derive(Serialize, Deserialize, Debug)]
+
+pub enum MessageType {
+    Text(String),
+    Image(Vec<u8>),
+    File { name: String, content: Vec<u8> },
+}
+
+impl MessageType {
+    pub fn serialize(&self) -> Result<Vec<u8>, BincodeError> {
+        bincode::serialize(&self)
+    }
+    pub fn deserialize(from: &[u8]) -> Result<Self, BincodeError> {
+        bincode::deserialize(&from)
+    }
+}
+
+// pub fn add(left: usize, right: usize) -> usize {
+//     left + right
+// }
+
+// #[cfg(test)]
+// mod tests {
+//     use super::*;
+
+//     #[test]
+//     fn it_works() {
+//         let result = add(2, 2);
+//         assert_eq!(result, 4);
+//     }
+// }
